175d8399974eeeae200db9307a353cfefa5289d4
antonson <tf.2.98@mail.ru>
2017-04-24 11:34:39 +0300

    swipe to dismiss

--- FoodCritic/app/src/main/java/com/example/yoant/foodcritic/adapters/ProductAdapter.java
+++ FoodCritic/app/src/main/java/com/example/yoant/foodcritic/adapters/ProductAdapter.java
@@ -1,11 +1,14 @@
 package com.example.yoant.foodcritic.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.media.Image;
+import android.os.Handler;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,17 +17,28 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.yoant.foodcritic.R;
 import com.example.yoant.foodcritic.models.Product;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
 
-    private Product[] mProductList;
+    private static final int REMOVE_TIMEOUT = 3000;
+
+    private List<Product> mProductList;
+    private List<Product> mItemsPendingRemove;
     private Context mContext;
     private List<Image> images;
     private String imgURL = "http://hitgid.com/images/%D1%84%D1%80%D1%83%D0%BA%D1%82%D1%8B-7.jpg";
 
+    private Handler handler = new Handler();
+    HashMap<Product, Runnable> pendingRunnables = new HashMap<>();
+
     public ProductAdapter(Product[] products, Context context){
-        mProductList = products;
+        mProductList = new ArrayList<>();
+        mItemsPendingRemove = new ArrayList<>();
+        mProductList.addAll(Arrays.asList(products));
         mContext = context;
     }
 
@@ -32,42 +46,131 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
     public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_product_representation, parent, false);
         final ProductViewHolder holder = new ProductViewHolder(view);
+        //view.setOnClickListener(null);
         return holder;
     }
 
     @Override
     public void onBindViewHolder(ProductViewHolder holder, int position) {
+        ProductViewHolder viewHolder = holder;
+        final Product product = mProductList.get(position);
 
-        Product product = mProductList[position];
-        holder.productImage.setImageResource(R.drawable.vitamins_fruit_logo);
-        Glide.with(mContext).load(imgURL).thumbnail(0.5f).crossFade().diskCacheStrategy(DiskCacheStrategy.ALL).into(holder.productImage);
 
-        holder.productName.setText(product.getName());
-        holder.productFat.setText("" + product.getFat());
-        holder.productProt.setText(""+product.getProtein());
-        holder.productEnergy.setText(""+product.getEnergeticValue());
-        holder.productCarb.setText(""+product.getCarb());
+
+        if(mItemsPendingRemove.contains(product)){
+            viewHolder.itemView.setBackgroundColor(Color.RED);
+            viewHolder.productName.setVisibility(View.GONE);
+            viewHolder.productImage.setVisibility(View.GONE);
+            viewHolder.productCarb.setVisibility(View.GONE);
+            viewHolder.carbDesc.setVisibility(View.GONE);
+            viewHolder.productFat.setVisibility(View.GONE);
+            viewHolder.fatDesc.setVisibility(View.GONE);
+            viewHolder.productProt.setVisibility(View.GONE);
+            viewHolder.protDesc.setVisibility(View.GONE);
+            viewHolder.productEnergy.setVisibility(View.GONE);
+            viewHolder.energyDesc.setVisibility(View.GONE);
+            viewHolder.undoButton.setVisibility(View.VISIBLE);
+
+            viewHolder.undoButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view){
+                    Runnable runnable = pendingRunnables.get(product);
+                    pendingRunnables.remove(product);
+                    if(pendingRunnables!=null)
+                        handler.removeCallbacks(runnable);
+                    mItemsPendingRemove.remove(product);
+                    notifyItemChanged(mProductList.indexOf(product));
+                }
+            });
+        } else {
+            viewHolder.itemView.setBackgroundColor(Color.WHITE);
+            viewHolder.productName.setVisibility(View.VISIBLE);
+            viewHolder.productImage.setVisibility(View.VISIBLE);
+            viewHolder.productCarb.setVisibility(View.VISIBLE);
+            viewHolder.carbDesc.setVisibility(View.VISIBLE);
+            viewHolder.productFat.setVisibility(View.VISIBLE);
+            viewHolder.fatDesc.setVisibility(View.VISIBLE);
+            viewHolder.productProt.setVisibility(View.VISIBLE);
+            viewHolder.protDesc.setVisibility(View.VISIBLE);
+            viewHolder.productEnergy.setVisibility(View.VISIBLE);
+            viewHolder.energyDesc.setVisibility(View.VISIBLE);
+            viewHolder.undoButton.setVisibility(View.GONE);
+            viewHolder.undoButton.setOnClickListener(null);
+            holder.productImage.setImageResource(R.drawable.vitamins_fruit_logo); //TODO check without it
+            //Glide.with(mContext).load(imgURL).thumbnail(0.5f).crossFade().diskCacheStrategy(DiskCacheStrategy.ALL).into(holder.productImage);
+            holder.productName.setText(product.getName());
+            holder.productFat.setText("" + product.getFat());
+            holder.productProt.setText(""+product.getProtein());
+            holder.productEnergy.setText(""+product.getEnergeticValue());
+            holder.productCarb.setText(""+product.getCarb());
+        }
+    }
+
+    public void pendingRemoval(int position){
+        final Product product = mProductList.get(position);
+        if(!mItemsPendingRemove.contains(product)){
+            mItemsPendingRemove.add(product);
+            notifyItemChanged(position);
+
+            Runnable pendingRemove = new Runnable() {
+                @Override
+                public void run() {
+                    remove(mProductList.indexOf(product));
+                }
+            };
+            handler.postDelayed(pendingRemove, REMOVE_TIMEOUT);
+            pendingRunnables.put(product, pendingRemove);
+        }
+    }
+
+    public void remove(int position){
+        Product product = mProductList.get(position);
+        if(mItemsPendingRemove.contains(product))
+            mItemsPendingRemove.remove(product);
+        if(mProductList.contains(product)){
+            mProductList.remove(position);
+            notifyItemRemoved(position);
+        }
+    }
+
+    public boolean isPendingRemoval(int position){
+        Product product = mProductList.get(position);
+        return mItemsPendingRemove.contains(product);
     }
 
 
     public class ProductViewHolder extends RecyclerView.ViewHolder{
         public ImageView productImage;
-        public TextView productName, productFat, productCarb, productEnergy, productProt;
+        public TextView  productName;
+        public TextView fatDesc;
+        public TextView productFat;
+        public TextView carbDesc;
+        public TextView productCarb;
+        public TextView energyDesc;
+        public TextView productEnergy;
+        public TextView protDesc;
+        public TextView productProt;
+        public Button undoButton;
 
         public ProductViewHolder(View view){
             super(view);
             productImage = (ImageView)view.findViewById(R.id.single_product_representation_image);
             productName = (TextView)view.findViewById(R.id.single_product_representation_name);
+            carbDesc = (TextView)view.findViewById(R.id.single_product_representation_carb);
             productCarb = (TextView)view.findViewById(R.id.single_product_representation_carb_value);
+            energyDesc = (TextView)view.findViewById(R.id.single_product_representation_energy);
             productEnergy = (TextView)view.findViewById(R.id.single_product_representation_energy_value);
+            fatDesc = (TextView)view.findViewById(R.id.single_product_representation_fat);
             productFat = (TextView)view.findViewById(R.id.single_product_representation_fat_value);
+            protDesc = (TextView)view.findViewById(R.id.single_product_representation_prot);
             productProt = (TextView)view.findViewById(R.id.single_product_representation_prot_value);
+            undoButton = (Button)view.findViewById(R.id.undo_button);
         }
     }
 
     @Override
     public int getItemCount() {
-        return mProductList.length;
+        return mProductList.size();
     }
 
 }
--- FoodCritic/app/src/main/java/com/example/yoant/foodcritic/view/activities/ProductsActivity.java
+++ FoodCritic/app/src/main/java/com/example/yoant/foodcritic/view/activities/ProductsActivity.java
@@ -1,86 +1,238 @@
 package com.example.yoant.foodcritic.view.activities;
 
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.yoant.foodcritic.R;
 import com.example.yoant.foodcritic.adapters.ProductAdapter;
 import com.example.yoant.foodcritic.adapters.RecyclerViewItemClickListener;
+import com.example.yoant.foodcritic.helper.DividerItemDecoration;
 import com.example.yoant.foodcritic.helper.sqlite.SQLiteDatabaseHelper;
 import com.example.yoant.foodcritic.models.Product;
-import com.example.yoant.foodcritic.helper.DividerItemDecoration;
 import com.example.yoant.foodcritic.view.fragments.ProductDetailFragment;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class ProductsActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
+    private RecyclerView mRecyclerView;
+    private Product[] mProducts1;
+    private SQLiteDatabaseHelper mDatabase;
+    private FloatingActionButton mFloatButton;
 
+    /*
+    The values of protein, fat, carb, energy must be double and with MAX 2 digits after dot
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         final FragmentManager fragmentManager = getSupportFragmentManager();
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_products_activity);
+        mFloatButton = (FloatingActionButton) findViewById(R.id.fab);
 
+        mDatabase = SQLiteDatabaseHelper.getsInstance(getApplicationContext());
         setSupportActionBar(toolbar);
-        recyclerView = (RecyclerView)findViewById(R.id.recycler_products_activity);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
-        SQLiteDatabaseHelper db = SQLiteDatabaseHelper.getsInstance(getApplicationContext());
-        Product[] products2 = Product.products;
-        for(Product product : products2)
-            db.addProduct(product);
-        List<Product> products = db.getAllProducts();
-        Product[] products1 = new Product[products.size()];
-        int i = 0;
-        for(Product product : products)
-            products1[i++] = product;
-        ProductAdapter adapter = new ProductAdapter(products1, getApplicationContext());
-//        adapter.setHasStableIds(true);
-        recyclerView.setAdapter(adapter);
+        setDataForAdapter();
+        setUpRecyclerView();
 
-        recyclerView.addOnItemTouchListener(new RecyclerViewItemClickListener(getApplicationContext(), recyclerView, new RecyclerViewItemClickListener.OnItemClickListener() {
+        mRecyclerView.addOnItemTouchListener(new RecyclerViewItemClickListener(getApplicationContext(), mRecyclerView, new RecyclerViewItemClickListener.OnItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
                 ProductDetailFragment productDetailFragment = new ProductDetailFragment();
-                Bundle bundle = new Bundle();
-                bundle.putString("name", "Arbuz");
-                bundle.putString("info", "BLA \n BLA \n BLA \n BLA \n BLA \n BLA \n BLA \n BLA \n " +
-                        "BLA \n BLA \n BLA \n BLA \n BLA \n BLA \n " +
-                        "BLA \n BLA \n BLA \n BLA \n BLA \n BLA \n BLA \n BLA \n " +
-                        "BLA \n BLA \n BLA \n BLA \n BLA \n BLA \n " +
-                        "BLA \n BLA \n BLA \n BLA \n BLA \n BLA \n BLA \n BLA \n " +
-                        "BLA \n BLA \n BLA \n BLA \n BLA \n BLA \n " +
-                        "BLA \n BLA \n BLA \n BLA \n BLA \n BLA \n BLA \n BLA \n " +
-                        "BLA \n BLA \n BLA \n BLA \n BLA \n BLA \n ");
+                Product product = mProducts1[position];
+                Bundle bundle = setUpBundleDetailProduct(product);
                 productDetailFragment.setArguments(bundle);
                 productDetailFragment.show(fragmentManager, "DialogFragment");
             }
 
             @Override
             public void onLongItemClick(View view, int position) {
-                Toast.makeText(getApplicationContext(), "Hello from long " + position, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Tipo Editing, Aga " + position, Toast.LENGTH_SHORT).show();
+            }
+
+            private Bundle setUpBundleDetailProduct(Product product) {
+                Bundle bundle = new Bundle();
+                bundle.putString("name", product.getName());
+                StringBuilder builder = new StringBuilder();
+                builder.append("Name: ").append(product.getName()).append('\n')
+                        .append(" Protein: ").append(product.getProtein()).append('\n')
+                        .append(" Fat: ").append(product.getFat()).append('\n')
+                        .append(" Carb: ").append(product.getCarb());
+                bundle.putString("info", builder.toString());
+                return bundle;
             }
         }));
 
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mFloatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), CreateProductActivity.class);
+                intent.setFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_NO_HISTORY);
                 startActivity(intent);
             }
         });
     }
+
+    private void setUpRecyclerView() {
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        ProductAdapter adapter = new ProductAdapter(mProducts1, getApplicationContext());
+        mRecyclerView.setAdapter(adapter);
+        mRecyclerView.setHasFixedSize(true);
+        setAnimationDecorator();
+        setUpItemTouchHelper();
+
+    }
+
+    private void setDataForAdapter() {
+        if (mDatabase.getAllProducts().isEmpty()) {
+            Product[] products2 = Product.products;
+            for (Product product : products2)
+                mDatabase.addProduct(product);
+        }
+        List<Product> products = mDatabase.getAllProducts();
+        mProducts1 = new Product[products.size()];
+        for (Product product : products)
+            mProducts1[products.indexOf(product)] = product;
+    }
+
+    private void setUpItemTouchHelper() {
+        ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            Drawable background;
+            Drawable deleteMark;
+            int deleteMarkMargin;
+            boolean isInitialised;
+
+            private void initialise() {
+                background = new ColorDrawable(Color.RED);
+                deleteMark = ContextCompat.getDrawable(ProductsActivity.this, R.drawable.delete_24dp);
+                deleteMark.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+                deleteMarkMargin = (int) ProductsActivity.this.getResources().getDimension(R.dimen.delete_margin)+20;
+                isInitialised = true;
+            }
+
+            @Override
+            public int getSwipeDirs(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                int position = viewHolder.getAdapterPosition();
+                ProductAdapter productAdapter = (ProductAdapter) recyclerView.getAdapter();
+                if (productAdapter.isPendingRemoval(position)) //TODO do this method
+                    return 0;
+                return super.getSwipeDirs(recyclerView, viewHolder);
+            }
+
+
+            @Override
+            public void onChildDraw(Canvas canvas, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isActiveNow) {
+                View itemView = viewHolder.itemView;
+
+                if (viewHolder.getAdapterPosition() == -1)
+                    return;
+
+                if (!isInitialised)
+                    initialise();
+
+                background.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+                background.draw(canvas);
+
+                int itemHeight = itemView.getBottom() - itemView.getTop();
+                int intrinsicWidth = deleteMark.getIntrinsicWidth();
+                int intrinsicHeight = deleteMark.getIntrinsicWidth();
+
+                int deleteMarkLeft = itemView.getRight() - intrinsicWidth - deleteMarkMargin;
+                int deleteMarkRight = itemView.getRight() - deleteMarkMargin;
+                int deleteMarkTop = itemView.getTop() + (itemHeight - intrinsicHeight) / 2;
+                int deleteMarkBottom = deleteMarkTop + intrinsicHeight;
+                deleteMark.setBounds(deleteMarkLeft, deleteMarkTop, deleteMarkRight, deleteMarkBottom);
+
+                deleteMark.draw(canvas);
+
+                super.onChildDraw(canvas, recyclerView, viewHolder, dX, dY, actionState, isActiveNow);
+            }
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+                ProductAdapter productAdapter = (ProductAdapter) mRecyclerView.getAdapter();
+                productAdapter.pendingRemoval(position);
+            }
+        };
+        ItemTouchHelper mItemTouchHelper = new ItemTouchHelper(simpleCallback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
+    }
+
+    private void setAnimationDecorator() {
+        mRecyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+
+            Drawable background;
+            boolean isInitialised;
+
+            private void initialise() {
+                background = new ColorDrawable(Color.RED);
+                isInitialised = true;
+            }
+
+            @Override
+            public void onDraw(Canvas canvas, RecyclerView parent, RecyclerView.State state){
+                if(!isInitialised)
+                    initialise();
+
+                if(parent.getItemAnimator().isRunning()){
+                    View firstViewUp = null;
+                    View lastViewDown = null;
+
+                    int left = 0;
+                    int right = parent.getWidth();
+                    int top = 0;
+                    int bottom = 0;
+
+                    int childCount = parent.getLayoutManager().getChildCount();
+                    for(int i = 0; i < childCount; i++){
+                        View child = parent.getLayoutManager().getChildAt(i);
+                        if(child.getTranslationY() < 0)
+                            lastViewDown = child;
+                        else if(child.getTranslationY() > 0)
+                            if(firstViewUp == null)
+                                firstViewUp = child;
+                    }
+
+                    if(lastViewDown != null && firstViewUp != null){
+                        top = lastViewDown.getBottom() + (int)lastViewDown.getTranslationY();
+                        bottom = firstViewUp.getTop() + (int)firstViewUp.getTranslationY();
+                    } else if(lastViewDown != null){
+                        top = lastViewDown.getBottom() + (int)lastViewDown.getTranslationY();
+                        bottom = lastViewDown.getBottom();
+                    } else if(firstViewUp != null){
+                        top = firstViewUp.getTop();
+                        bottom = firstViewUp.getTop() + (int)firstViewUp.getTranslationY();
+                    }
+                    background.setBounds(left, top, right, bottom);
+                    background.draw(canvas);
+                }
+                super.onDraw(canvas, parent, state);
+            }
+        });
+    }
+
 }
